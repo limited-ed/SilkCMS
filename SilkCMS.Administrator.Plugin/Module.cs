4727bc130d65e8b6d99f7750ed5832f6529c4220
limited-ed <yarosroman@gmail.com>
2026-02-23 20:13:31 +0800

    Dashboard

--- SilkCMS.Administrator.Plugin/Module.cs
+++ SilkCMS.Administrator.Plugin/Module.cs
@@ -32,16 +32,21 @@ public class AdministratorModule : IModule
         
         var items = new List<MenuItem>()
         {
-            new() { Order = 1, Title = "General", IsHeader = true, Children =
+            new MenuItem(){Order = 1, Title = "Content Management", IsHeader = true},
+            new() { Order = 1, Title = "Content", Icon="lni-paperclip-1", Children =
                 [
-                    new() { Order = 1, Title = "Settings" },
-                    new() { Order = 2, Title = "Plugins" },
-                    new() { Order = 3, Title = "Documentation" },
-                    new() { Order = 4, Title = "About" }
+                    new() { Order = 1, Title = "Content types" },
+                    new() { Order = 2, Title = "Add" },
                 ]
             },
-            new() { Order = 2, IsSeparator = true},
-            new() { Order = 2, Title = "Users Administration", IsHeader = true, Children = 
+            new() { Order = 1, Title = "Media", Icon="lni-paperclip-1", Children =
+                [
+                    new() { Order = 1, Title = "Media gallery" },
+                    new() { Order = 2, Title = "Upload media" },
+                ]
+            },
+            new() { Order = 2, Title = "Security", IsHeader = true},
+            new() { Order = 2, Title = "Users Administration", Icon = "lni-user-multiple-4" ,Children = 
                 [
                     new ()
                     {
@@ -58,10 +63,17 @@ public class AdministratorModule : IModule
                             new() {Order = 1, Title = "Groups Administration"}
                         ]
                     },
-                    new() {Order = 3, Title = "Registration providers", IsHeader = true}
-                    
                 ]
-            }
+            },
+            new MenuItem(){Order = 1, Title = "Content Management", IsHeader = true},
+            new() { Order = 1, Title = "General", Icon="lni-paperclip-1", Children =
+                [
+                    new() { Order = 1, Title = "Settings" },
+                    new() { Order = 2, Title = "Plugins" },
+                    new() { Order = 3, Title = "Documentation" },
+                    new() { Order = 4, Title = "About" }
+                ]
+            },
         };
         var mb = app.Services.GetService<MenuBuilder.MenuBuilder>();
         mb.AddItems(items);
